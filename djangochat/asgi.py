afceab9d99fe2ceb7d67e0c3d113e7db1ef6978a
Tikhonov-86 <124086407+Tikhonov-86@users.noreply.github.com>
2024-10-30 00:58:38 +0500

    Some edit 30.10.24

--- djangochat/asgi.py
+++ djangochat/asgi.py
@@ -4,7 +4,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import room.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangochat.settings')  
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- djangochat/settings.py
+++ djangochat/settings.py
@@ -77,16 +77,13 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'djangochat.wsgi.application'
-ASGI_APPLICATION = 'djangochat.asgi.application'
+ASGI_APPLICATION = "djangochat.asgi.application"
 
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-            'CONFIG': {
-                "hosts": [('127.0.0.1', 6379)],
-            },
-        },
-    }
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    },
+}
 
 
 # Database
