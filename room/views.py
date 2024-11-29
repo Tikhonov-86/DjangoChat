72800113704708d6400a0d147fd4df1eeff4c637
Tikhonov-86 <124086407+Tikhonov-86@users.noreply.github.com>
2024-11-29 10:20:53 +0500

    Edit 29.11.24

--- room/views.py
+++ room/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Message, Room
 
@@ -9,11 +11,22 @@ def rooms(request):
 
     return render(request, 'room/rooms.html', {'rooms': rooms})
 
+
 @login_required
 def room(request, slug):
     room = Room.objects.get(slug=slug)
-    messages = Message.objects.filter(room=room)[0:25]
 
+    if request.method == "POST":
+        content = request.POST.get("content", "").strip()
+        if content:
+            Message.objects.create(
+                room=room,
+                user=request.user,
+                content=content,
+            )
+        return HttpResponseRedirect(reverse('room', args=[slug]))
+
+    messages = Message.objects.filter(room=room)[0:25]
     return render(request, 'room/room.html', {'room': room, 'messages': messages})
 
 
